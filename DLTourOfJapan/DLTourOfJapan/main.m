0cae23790f963d9baa5913b7fe242e2b7e19f37f
ChinaPicture <576414408@qq.com>
2014-06-16 10:11:35 +0800

    adfs

---
+++ DLTourOfJapan/DLTourOfJapan/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DLTourOfJapan
+//
+//  Created by developer on 14-6-16.
+//  Copyright (c) 2014年 duostec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZXYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZXYAppDelegate class]));
+    }
+}
